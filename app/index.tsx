cba38c72f7c3aa01ecfc22adda888f883b5ba739
Waggerok <115732069+Waggerok@users.noreply.github.com>
2025-04-22 22:57:01 +0300

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { AntDesign } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+      <AntDesign name="stepforward" size={24} color="black" />
+      <Text className="text-2xl">Edit app/index.asdasdasdtsx ssto edit this screen.</Text>
+    </View>
+  );
+}
